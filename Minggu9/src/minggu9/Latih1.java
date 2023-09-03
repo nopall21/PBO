4efb3027d34c96b019e70bcc631fd596b0468e99
nopall21 <92071010+nopall21@users.noreply.github.com>
2023-09-03 08:52:58 +0800

    Add files via upload

---
+++ Minggu9/src/minggu9/Latih1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minggu9;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Naufal
+ */
+
+public class Latih1 extends Application
+{
+    public static void main(String[] args)
+    {
+    launch (args);
+    }
+    
+    public void start(Stage theStage)
+    {
+        theStage.setTitle("Hello World");
+        theStage.show();
+    }
+}
